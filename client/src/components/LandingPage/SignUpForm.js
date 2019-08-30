cca47d2b0d0b193bec7f38d3b019a9f7dba6f6a5
Muhammad Sarim <muhammad.sarim@clouddx.com>
2019-08-30 17:10:10 -0400

    changing link on signup form

--- client/src/components/LandingPage/SignUpForm.js
+++ client/src/components/LandingPage/SignUpForm.js
@@ -133,7 +133,7 @@ export default function SignUpForm() {
                     </Button>
                     <Grid container justify="flex-end">
                         <Grid item>
-                            <Link href="#" variant="body2">
+                            <Link href="/signin" variant="body2">
                                 Already have an account? Sign in
                             </Link>
                         </Grid>
